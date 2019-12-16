4650fab96c41b444c04da6910ed9751dc483a278
hereisfahad <fahadjabbar31@gmail.com>
2019-12-17 03:19:24 +0500

    Updates

---
+++ precache-manifest.ea6e80692d6f3150398cae15de692b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439c9a22c27cc7d4c1b27428528e9be1",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "996ae4ad49473f5a0cb3",
+    "url": "/weatherApp/static/css/main.0811f275.chunk.css"
+  },
+  {
+    "revision": "2d518d8515cf53863e2d",
+    "url": "/weatherApp/static/js/2.58883166.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/weatherApp/static/js/2.58883166.chunk.js.LICENSE"
+  },
+  {
+    "revision": "996ae4ad49473f5a0cb3",
+    "url": "/weatherApp/static/js/main.9872ab30.chunk.js"
+  },
+  {
+    "revision": "b1e9a24928adf9e1b54a",
+    "url": "/weatherApp/static/js/runtime-main.1ee812be.js"
+  },
+  {
+    "revision": "805d840651e130629f8367b4e5b73b85",
+    "url": "/weatherApp/static/media/location.805d8406.svg"
+  }
+]);
