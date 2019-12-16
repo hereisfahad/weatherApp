ae275fce7ff78714bfe0286277c954e5b87c503b
hereisfahad <fahadjabbar31@gmail.com>
2019-12-17 02:38:22 +0500

    get geolocation based weather update

--- src/App.js
+++ src/App.js
@@ -1,70 +1,121 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { ReactComponent as Pin } from "./location.svg";
+import "./App.css";
 
 class App extends Component {
-    state = {
-        temp: '',
-        humd: '',
-        image:'',
-        loc:'',
-        error:''
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp: "",
+      humd: "",
+      image: "",
+      loc: "",
+      error: ""
+    };
+  }
+  handleClick = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    try {
+      const api_call = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&apikey=dbd802d470137cd4c005f9625b564ea2`
+      );
+      const data = await api_call.json();
+      this.setState({
+        temp: data.main.temp,
+        humd: data.main.humidity,
+        image: `https://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+        loc: data.name,
+        error: ""
+      });
+    } catch (error) {
+      this.setState({
+        temp: "",
+        humd: "",
+        image: "",
+        loc: "",
+        error: "Please enter the correct city and country names"
+      });
     }
-    handleClick = async (e) => {
-        e.preventDefault();
-//        console.log(e.target.elements.city.value)
-        const city = e.target.elements.city.value;
-        const country = e.target.elements.country.value;
-        try{
-            const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&apikey=dbd802d470137cd4c005f9625b564ea2`);
-            const data = await api_call.json();
-             this.setState({
-                temp: data.main.temp,
-                humd: data.main.humidity,          
-                image: `https://openweathermap.org/img/w/${data.weather[0].icon}.png`,
-                loc: data.name,
-                error: ''   
-            })
-        }catch(error){
+  };
+  getGeo = async () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&apikey=dbd802d470137cd4c005f9625b564ea2`
+        )
+          .then(data => data.json())
+          .then(data => {
             this.setState({
-                temp: '',
-                humd: '',
-                image:'',
-                loc:'',
-                error: 'Please enter the correct city and country names'
-            })
+              temp: data.main.temp,
+              humd: data.main.humidity,
+              image: `https://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+              loc: data.name,
+              error: ""
+            });
+          });
+      });
+    } else {
+      alert("Sorry, browser does not support geolocation!");
     }
-//        console.log(data);
-//        console.log(data.weather[0].icon);
-//        console.log(this.state.image);
-      }
+  };
   render() {
     const { image, loc, temp, humd, error } = this.state;
     return (
       <div className="container">
         <center>
-            <div className ="card" id="card1">
+          <div className="card" id="card1">
+            <span id="title">Weather</span>
 
-                <span id = "title">Weather</span>
-
-                <form onSubmit = {this.handleClick} >
-                    <input type="text" placeholder="city"  name ="city" className = "form-control"/> <br></br><br></br>
-                    <input type="text" placeholder="country"  name ="country" className = "form-control"/> <br></br><br></br>
-                    <button className = "btn" > Get Weather </button>
-                </form>
-                <span id= "error">{error!==''?<h1>Error:{error}</h1>:''}</span>
+            <form onSubmit={this.handleClick}>
+              <input
+                type="text"
+                placeholder="city"
+                name="city"
+                className="form-control"
+              />{" "}
+              <br></br>
+              <br></br>
+              <input
+                type="text"
+                placeholder="country"
+                name="country"
+                className="form-control"
+              />{" "}
+              <br></br>
+              <br></br>
+              <button className="btn"> Get Weather </button>
+            </form>
+            <Pin
+              style={{
+                height: "100%",
+                width: "75px",
+                padding: "12px 0px"
+              }}
+              onClick={this.getGeo}
+            />
+            <span id="error">{error !== "" ? <h1>Error:{error}</h1> : ""}</span>
+          </div>
+          {image !== "" ? (
+            <div className="card" id="res">
+              {image !== "" ? (
+                <img
+                  src={image}
+                  width="100px"
+                  height="100px"
+                  alt="weather icon"
+                ></img>
+              ) : (
+                ""
+              )}
+              {loc !== "" ? <h1>City: {loc}</h1> : ""}
+              {temp !== "" ? <h1>Temp: {temp}</h1> : ""}
+              {humd !== "" ? <h1>Humidity: {humd}</h1> : ""}
             </div>
-            {
-              image!==''
-              ?
-              <div className="card" id ="res">
-                    {image!==''?<img src ={image} width = "100px" height = "100px" alt = "weather icon"></img>:''}
-                    {loc!==''?<h1>City: {loc}</h1>:''}
-                    {temp!==''?<h1>Temp: {temp}</h1>:''}
-                    {humd!==''?<h1>Humidity: {humd}</h1>:''}
-              </div>
-              : ''
-            }
-
+          ) : (
+            ""
+          )}
         </center>
       </div>
     );
